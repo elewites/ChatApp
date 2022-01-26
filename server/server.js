2f4b9358ed5bf85c8981099ea32eec47c030317f
Eros Rodriguez <elewites@gmail.com>
2022-01-25 21:02:23 -0800

    added database connection

--- server/server.js
+++ server/server.js
@@ -1,19 +1,36 @@
 //libraries
 const express = require("express");
 const app = express();
-const socket = require("socket.io");
+const http = require("http");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const { Server } = require("socket.io");
 
 //Middleware
 app.use(cors());
+dotenv.config();
 
 //App setup
-const server = app.listen(3001, () =>
-  console.log("listening to requests on port 3001")
+const PORT = process.env.PORT || 3001;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log("sever is running");
+});
+
+//database connection
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_username}:${process.env.DB_password}@cluster0.exjg5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  () => {
+    console.log("connected to database");
+  }
 );
 
 //Socket setup
-const io = socket(server, { cors: { origin: "*" } });
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
 
 io.on("connection", (socket) => {
   console.log("socket id: " + socket.id);
